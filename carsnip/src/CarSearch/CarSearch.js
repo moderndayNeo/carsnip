55ccb73addc0404dee4d968de3f9a6dfeda5aae3
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-05-31 19:28:14 +0100

    feat: Create CarouselWrapper

--- carsnip/src/CarSearch/CarSearch.js
+++ carsnip/src/CarSearch/CarSearch.js
@@ -2,12 +2,14 @@ import React from 'react'
 import './CarSearch.css'
 import CarsnipLogo from './CarsnipLogo/CarsnipLogo'
 import SearchWrapper from './SearchWrapper/SearchWrapper'
+import CarouselWrapper from './CarouselWrapper/CarouselWrapper'
 
 export default function CarSearch() {
     return (
         <div className="CarSearch">
             <CarsnipLogo />
             <SearchWrapper />
+            <CarouselWrapper />
         </div>
     )
 }
---
+++ carsnip/src/CarSearch/CarouselWrapper/CarouselWrapper.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './CarouselWrapper.css'
+
+export default function CarouselWrapper() {
+    return (
+        <div className="CarouselWrapper">
+            
+        </div>
+    )
+}
