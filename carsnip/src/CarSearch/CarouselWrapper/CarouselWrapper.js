e1e3187f645abe67c8daf41e50245807e24a343c
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-05-31 19:29:19 +0100

    feat: Create Scroller

--- carsnip/src/CarSearch/CarouselWrapper/CarouselWrapper.js
+++ carsnip/src/CarSearch/CarouselWrapper/CarouselWrapper.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import './CarouselWrapper.css'
+import Scroller from './Scroller/Scroller'
 
 export default function CarouselWrapper() {
     return (
         <div className="CarouselWrapper">
-            
+            <Scroller />
         </div>
     )
 }
---
+++ carsnip/src/CarSearch/CarouselWrapper/Scroller/Scroller.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Scroller.css'
+
+export default function Scroller() {
+    return (
+        <div className="Scroller">
+            
+        </div>
+    )
+}
