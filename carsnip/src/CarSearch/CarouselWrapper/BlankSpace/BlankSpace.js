ef1c12e597089b6ba16f209d78bd97353b4a354c
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-05-31 21:05:53 +0100

    feat: Create BlankSpace

---
+++ carsnip/src/CarSearch/CarouselWrapper/BlankSpace/BlankSpace.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './BlankSpace.css'
+
+export default function BlankSpace() {
+    return (
+        <div className="BlankSpace">
+            
+        </div>
+    )
+}
--- carsnip/src/CarSearch/CarouselWrapper/CarouselWrapper.js
+++ carsnip/src/CarSearch/CarouselWrapper/CarouselWrapper.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './CarouselWrapper.css'
 import Scroller from './Scroller/Scroller'
+import BlankSpace from './BlankSpace/BlankSpace'
 
 export default function CarouselWrapper() {
     return (
         <div className="CarouselWrapper">
+            <BlankSpace />
             <Scroller />
         </div>
     )
