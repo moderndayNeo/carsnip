0dae59150afcec33145cae0e15a209337142d34a
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-05-31 18:35:23 +0100

    feat: Create SearchWrapper

--- carsnip/src/CarSearch/CarSearch.js
+++ carsnip/src/CarSearch/CarSearch.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './CarSearch.css'
 import CarsnipLogo from './CarsnipLogo/CarsnipLogo'
+import SearchWrapper from './SearchWrapper/SearchWrapper'
 
 export default function CarSearch() {
     return (
         <div className="CarSearch">
             <CarsnipLogo />
+            <SearchWrapper />
         </div>
     )
 }
