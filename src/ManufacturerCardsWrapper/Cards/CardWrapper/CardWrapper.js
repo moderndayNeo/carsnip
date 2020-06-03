7b32fcf5ba2cc02d821fae25c7a03b68cb375211
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-06-03 12:23:15 +0100

    feat: Desktop query for ManufacturerList

--- src/ManufacturerCardsWrapper/Cards/CardWrapper/CardWrapper.js
+++ src/ManufacturerCardsWrapper/Cards/CardWrapper/CardWrapper.js
@@ -2,7 +2,6 @@ import React from 'react'
 import './CardWrapper.css'
 
 export default function CardWrapper({ href, src, cardName, cardCount }) {
-
     return (
         <a href={`/used-cars/by/${cardName}`} className="CardWrapper">
             <img src={src} alt={cardName} />
