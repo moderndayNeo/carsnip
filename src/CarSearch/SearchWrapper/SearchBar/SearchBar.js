9069ea1ef4733d1e880fc1fc12fcde3981f0785c
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-06-04 14:06:33 +0100

    feat: Intro ReactRotatingText

--- src/CarSearch/SearchWrapper/SearchBar/SearchBar.js
+++ src/CarSearch/SearchWrapper/SearchBar/SearchBar.js
@@ -3,13 +3,37 @@ import './SearchBar.css'
 import SpeechButton from './SpeechButton/SpeechButton'
 import SearchBox from './SearchBox/SearchBox'
 import SearchButton from './SearchButton/SearchButton'
+import ReactRotatingText from 
 
 export default function SearchBar() {
     return (
         <form role="search" method="get" action="/search" className="SearchBar">
             <SpeechButton />
+
+            {!search && (
+                <ReactRotatingText
+                    items={[
+                        'BMW X4 £14k Black',
+                        'Petrol Hatchback Manual £12k',
+                        '5 Door Petrol London Automatic',
+                        '4x4 70,000 miles £15k Cardiff',
+                        'Sports Convertible Blue £21k',
+                    ]}
+                    cursor=""
+                    pause="5000"
+                    emptyPause="1000"
+                    className="ReactRotatingText"
+                    typingInterval="150"
+                    deletingInterval="60"
+                />
+            )}
+
             <SearchBox />
+
             <SearchButton />
         </form>
     )
 }
+
+
+
