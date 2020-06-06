0400fcb44da28a00a1b7767493450d8ee5d3b546
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-06-06 14:53:42 +0100

    feat: Complete SearchBar loading animation

--- src/MainPage/CarSearch/SearchWrapper/SearchBar/SearchBox/SearchBox.js
+++ src/MainPage/CarSearch/SearchWrapper/SearchBar/SearchBox/SearchBox.js
@@ -2,10 +2,6 @@ import React from 'react'
 import './SearchBox.css'
 
 export default function SearchBox({ onChange, search, loading }) {
-    const placeholder = 'All the cars in one place...'
-    const searchBoxIsFocused = () => document.activeElement.className === "SearchBox"
-    console.log(loading)
-    console.log(loading && !searchBoxIsFocused())
 
     return (
         <input
@@ -14,7 +10,6 @@ export default function SearchBox({ onChange, search, loading }) {
             autoFocus={true}
             className="SearchBox"
             value={search}
-            // placeholder={ loading && !searchBoxIsFocused() ? placeholder : '' }
             placeholder={''}
             onChange={onChange}
         />
