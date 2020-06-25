398b003c5803f2fd15eb8e2d5db1819419255746
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-06-25 11:57:06 +0100

    feat: Copy Tooltip styles to Car component

--- src/MainPage/CarSearch/CarouselWrapper/CarouselContainer/Scroller/Car/Car.js
+++ src/MainPage/CarSearch/CarouselWrapper/CarouselContainer/Scroller/Car/Car.js
@@ -24,12 +24,11 @@ export default function Car({ alt, src }) {
                                 cx={13}
                                 cy="12.9"
                                 r={5}
-                                className="PulseDot__Circle"
                             />
-                            <g className="PulseDot__G">
+                            <g className="PulseDot-G">
                                 <path
                                     d="M13,7.9c2.8,0,5,2.2,5,5s-2.2,5-5,5s-5-2.2-5-5S10.2,7.9,13,7.9 M13-0.1c-7.2,0-13,5.8-13,13s5.8,13,13,13 s13-5.8,13-13S20.2-0.1,13-0.1L13-0.1z"
-                                    className="PulseDot__Path"
+                                    className="PulseDot-Path"
                                 />
                             </g>
                         </g>
@@ -38,12 +37,11 @@ export default function Car({ alt, src }) {
                                 cx={13}
                                 cy="12.9"
                                 r={5}
-                                className="PulseDot__Circle"
                             />
-                            <g className="PulseDot__G">
+                            <g className="PulseDot-G">
                                 <path
                                     d="M13,7.9c2.8,0,5,2.2,5,5s-2.2,5-5,5s-5-2.2-5-5S10.2,7.9,13,7.9 M13,3.9c-5,0-9,4-9,9s4,9,9,9s9-4,9-9 S18,3.9,13,3.9L13,3.9z"
-                                    className="PulseDot__Path"
+                                    className="PulseDot-Path"
                                 />
                             </g>
                         </g>
